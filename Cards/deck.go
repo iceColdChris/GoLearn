fa7251a31a7c52d2e6303e31929390c6a5f8815b
iceColdChris <chris_fahlin@comcast.net>
2017-08-26 00:11:35 -0700

    Finished up the cards tutorial, minus the testing will do testing tutorial later

--- Cards/deck.go
+++ Cards/deck.go
@@ -2,6 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
 )
 
 //Create deck type which is a slice of strings
@@ -29,4 +34,36 @@ func (d deck) print() {
 
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(fileName string) error {
+	return ioutil.WriteFile(fileName, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(fileName string) deck {
+	bs, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	s := strings.Split(string(bs), ",")
+	return deck(s)
+}
+
+func (d deck) shuffle() {
+
+	//Generating a seed based off of the time
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+
+	for i := range d {
+		n := r.Intn(len(d) - 1)
+
+		//Swap notation
+		d[i], d[n] = d[n], d[i]
+	}
 }
