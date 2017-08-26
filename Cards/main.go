27490ff3e5f15c6ea5605c2f1a4ba4bad934bb0c
iceColdChris <chris_fahlin@comcast.net>
2017-08-25 22:16:42 -0700

    loop-the-loop and slices

--- Cards/main.go
+++ Cards/main.go
@@ -8,10 +8,13 @@ func main() {
 	//These next two lines are the exact same
 	// var card string = "Ace of Spades"
 	//card := "Ace of Spades"
+	cards := []string{newCard(), "Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades")
 
-	card := newCard()
-
-	fmt.Println(card)
+	fmt.Println(cards)
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
 }
 
 func newCard() string {
