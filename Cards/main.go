7344907c42fd905fa1d9092256cf7ba7050e37e4
iceColdChris <chris_fahlin@comcast.net>
2017-08-25 22:05:23 -0700

    Functions and return types

--- Cards/main.go
+++ Cards/main.go
@@ -5,6 +5,15 @@ import (
 )
 
 func main() {
-	var card string = "Ace of Spades"
+	//These next two lines are the exact same
+	// var card string = "Ace of Spades"
+	//card := "Ace of Spades"
+
+	card := newCard()
+
 	fmt.Println(card)
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
