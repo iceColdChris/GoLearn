ba60209fdd80e97beb958eec00b2f3fdb86a9c2f
iceColdChris <chris_fahlin@comcast.net>
2017-08-25 22:37:59 -0700

    Recievers and custom types

--- Cards/main.go
+++ Cards/main.go
@@ -1,20 +1,10 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	//These next two lines are the exact same
-	// var card string = "Ace of Spades"
-	//card := "Ace of Spades"
-	cards := []string{newCard(), "Ace of Diamonds", newCard()}
+	cards := deck{newCard(), "Ace of Diamonds", newCard()}
 	cards = append(cards, "Six of Spades")
 
-	fmt.Println(cards)
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
+	cards.print()
 }
 
 func newCard() string {
