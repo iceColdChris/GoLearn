cc98e26eca5ce0c9730b9b1dd2476984a93f1d18
iceColdChris <chris_fahlin@comcast.net>
2017-08-26 22:07:04 -0700

    Added an interface to allow the program to run

--- Interfaces/main.go
+++ Interfaces/main.go
@@ -4,11 +4,23 @@ import (
 	"fmt"
 )
 
+type bot interface {
+	getGreeting() string
+}
+
 type englishBot struct{}
 type spanishBot struct{}
 
 func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
 
+	printGreeting(eb)
+	printGreeting(sb)
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
 }
 
 //Omit the value cause its unused
@@ -18,12 +30,4 @@ func (englishBot) getGreeting() string {
 
 func (spanishBot) getGreeting() string {
 	return "Hola!"
-}
-
-func printGreeting(eb englishBot) {
-	fmt.Println(eb.getGreeting())
-}
-
-func printGreeting(sb spanishBot) {
-	fmt.Println(sb.getGreeting())
 }
